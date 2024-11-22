b6b8519d8263ada1c6f0ce6c6be0f7d5f35e0556
Andrew Gerst <andrew.gerst@checkr.com>
2024-11-21 17:07:08 -0800

    add version 2 of make change script

--- ProgrammingChallenges/make_change.py
+++ ProgrammingChallenges/make_change.py
@@ -29,8 +29,34 @@ def make_change(change_amount):
 
   return [pennies, nickels, dimes, quarters]
 
+print('\nVersion 1')
+
 print(make_change(26) == [1, 0, 0, 1])
 print(make_change(26))
 
 print(make_change(45) == [0, 0, 2, 1])
 print(make_change(45))
+
+def make_change_v2(change_amount):
+  coins = [25, 10, 5, 1]
+  sorted_coins = sorted(coins)
+  num_of_coin_types = len(coins)
+  output = [0] * num_of_coin_types
+
+  for index, coin_value in enumerate(coins):
+    if change_amount < coin_value:
+      continue
+
+    coin_index = sorted_coins.index(coin_value)
+    output[coin_index] = change_amount // coin_value
+    change_amount -= coin_value * output[coin_index]
+
+  return output
+
+print('\nVersion 2')
+
+print(make_change_v2(26) == [1, 0, 0, 1])
+print(make_change_v2(26))
+
+print(make_change_v2(45) == [0, 0, 2, 1])
+print(make_change_v2(45))
